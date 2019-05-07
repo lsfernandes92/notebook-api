1d6f73bd794211b5c6f1bb387436f71a375cac9a
lsfernandes92 <lsfernandes92@gmail.com>
2019-05-07 17:39:33 -0300

    Overload the as_json to render the Contact from API

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,16 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind, optional: true
+
+   def author
+    "Lucas Fernandes"
+  end
+
+   def as_json(options={})
+    super(
+      root: true,
+      methods: :author,
+      except: [:created_at, :updated_at],
+      include: { kind: { except: [:created_at, :updated_at] } }
+    )
+  end
 end
