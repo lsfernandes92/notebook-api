27c9a1fc40252e6a37e90ca71e88b6241c2c3df1
lsfernandes92 <lsfernandes92@gmail.com>
2019-05-10 00:29:23 -0300

    Add best practice pradonization with associations 'belongs_to'
    Changes the way that contacts show the related links that they have with
    the kind it belongs through the route path 'contact_kind_path'.

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,6 @@
 class ContactSerializer < ActiveModel::Serializer
   belongs_to :kind, optional: true do
-    link(:related) { kind_url(object.kind.id) }
+    link(:related) { contact_kind_url(object.id) }
   end
   has_many :phones
   has_one :address
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :kinds
+  resources :contacts do
+    resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relationships/kind'
+  end
 
   root 'contacts#index'
 end
