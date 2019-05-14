737b9870716b8e60803143953c3e2ff651259043
lsfernandes92 <lsfernandes92@gmail.com>
2019-05-14 15:41:17 -0300

    Allow CRUD has_many from Phone

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,14 +1,43 @@
 class PhonesController < ApplicationController
-  before_action :set_phones
+  before_action :set_contacts
 
-  # GET /phones/1
+  def update
+    if @contact.phones.update(phone_params)
+      render json: @contact.phones, status: :created, location: contact_phones_url(@contact.id)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /contacts/1/phone
+  def destroy
+    phone = Phone.find(phone_params[:id])
+    phone.destroy
+  end
+
+  # POST contacts/1/phone
+  def create
+    @contact.phones << Phone.new(phone_params)
+
+    if @contact.save
+      render json: @contact.phones, status: :created, location: contact_phones_url(@contact.id)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  # GET /contacts/1/phones
   def show
-    render json: @phones
+    render json: @contact.phones
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_phones
-      @phones = Contact.find(params[:contact_id]).phones
+    def set_contacts
+      @contact = Contact.find(params[:contact_id])
+    end
+
+    def phone_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
